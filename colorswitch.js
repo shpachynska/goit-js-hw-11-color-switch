57014a409c40f1ed92aeb2c272a3c121d9781cc4
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2021-08-05 17:48:01 +0200

    changes files arrangement

--- colorswitch.js
+++ colorswitch.js
@@ -1,10 +1,10 @@
 const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
 ];
 
 const refs = {
@@ -18,17 +18,17 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-refs.startButton.addEventListener("click", () => {
-  refs.startButton.setAttribute("disabled", "disabled");
+refs.startButton.addEventListener('click', () => {
+  refs.startButton.setAttribute('disabled', 'disabled');
   intervalId = setInterval(() => {
     document.body.style.background = colors[randomIntegerFromInterval(0, 5)];
   }, 1000);
 });
 
-refs.stopButton.addEventListener("click", () => {
-  if (refs.startButton["disabled"]) {
+refs.stopButton.addEventListener('click', () => {
+  if (refs.startButton['disabled']) {
     clearInterval(intervalId);
-    refs.startButton.removeAttribute("disabled");
+    refs.startButton.removeAttribute('disabled');
   }
 });
 
